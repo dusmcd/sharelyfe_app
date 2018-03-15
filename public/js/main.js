af0c84932df0099b1ad4c85b6db776f4059b7905
dusmcd <dustinmcdowell1989@gmail.com>
2018-03-15 23:04:02 +0000

    add form validation

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,39 @@
+/*global $ */
+
 console.log('scripts are running on client side');
 // console.log(document.querySelector('#terms-conditions'));
 
 function validateForm() {
     let checkBox = document.querySelector('#terms-conditions');
+    let formErrors = document.querySelector('.form-errors');
+    let isValid = true;
+    let htmlString = '';
     
     if (!checkBox.checked) {
         alert('Must agree to the terms and conditions!');
-        return false;
+        isValid = false;
     }
+    if (document.querySelector('#first-name').value === '') {
+        htmlString += '<li>First name is a required field</li>';
+        isValid = false;
+    }
+    if (document.querySelector('#last-name').value === '') {
+        htmlString += '<li>Last name is a required field</li>';
+        isValid = false;
+    }
+    if (document.querySelector('#email').value === '') {
+        htmlString += '<li>Email is a required field</li>';
+        isValid = false;
+    }
+    if (document.querySelector('#username').value === '') {
+        htmlString += '<li>Username is a required field</li>';
+        isValid = false;
+    }
+    if (document.querySelector('#password').value === '') {
+        htmlString += '<li>Password is a required field</li>';
+        isValid = false;
+    }
+    
+    formErrors.innerHTML = htmlString;
+    return isValid;
 }
